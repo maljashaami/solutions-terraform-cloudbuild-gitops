805da8b2e028c7d74c415b398c0e3357b4546736
aljashaami <maz.aj@sada.com>
2022-08-24 19:25:11 +0000

    Update project IDs and buckets

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "maz-lab-tfstate"
     prefix = "env/prod"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="maz-lab"
